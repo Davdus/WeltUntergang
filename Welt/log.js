c9970d1380c046d75257711e36c965162b164299
Davdus <davlus143@gmail.com>
2017-03-24 12:02:50 +0100

    added another possible button script but with plain js

--- Welt/log.js
+++ Welt/log.js
@@ -6,3 +6,23 @@ $(document).ready(function(){
 });/**
  * Created by Davdus on 20.03.2017.
  */
+function onclickVariation(object){
+    //generiere array von class button und log
+    var buttons = document.getElementsByClassName("button");
+    var logs = document.getElementsByClassName("log");
+    var ii;
+    //loope durch um index des gedrückten buttons zu finden
+    for(i = 0; i < buttons.length; i++){
+        if(buttons[i] == object){
+            ii = i;
+            break;
+        }
+    }
+    //index von button = index von log weil sie nacheinander generiert werden
+    var target = logs[ii].style.display;
+    //prüfe den momentanen status und setze ihn auf den gegenteiligen
+    if(target == "none")
+        target = "block";
+    else
+        target = "none";
+}
